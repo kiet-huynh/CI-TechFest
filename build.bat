efd94773d24c966b7eebacd965e8793b4a37baf4
Kiet Huynh <kiet.huynh@headspring.com>
2015-09-11 17:47:36 -0500

    Update to include nuget package restore.

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo Executing psake build with default.ps1 configuration
 @echo off
 
+src\.nuget\nuget.exe install src\.nuget\packages.config -source "https://nuget.org/api/v2/" -RequireConsent -o "src\packages"
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0\src\packages\psake.4.4.2\tools\psake.ps1' %* -parameters @{"packageVersion"="'%2'"}; if ($psake.build_success -eq $false) { write-host "Build Failed!" -fore RED; exit 1 } else { exit 0 }" <nul
