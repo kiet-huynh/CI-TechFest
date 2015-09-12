0892e7edab20e1d9eda8d7cc90bfb967691d9664
Kiet Huynh <kiet.huynh@headspring.com>
2015-09-11 20:00:49 -0500

    Update with simple unit tests.

---
+++ src/CISample.Tests/BasicUnitTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CISample.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BasicUnitTest
+    {
+        [Test]
+        public void Should_Be_Equal()
+        {
+            Assert.AreEqual(1m, 1m);
+        }
+
+        [Test]
+        public void Should_Be_True()
+        {
+            Assert.IsTrue(true);
+        }
+        
+        //[Test]
+        //public void Should_Fail_Test()
+        //{
+        //    Assert.IsTrue(false);
+        //}
+    }
+}
